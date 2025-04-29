7d044e01633d8ca92ae34a035a5ce7d70a727dcd
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-04-29 13:46:11 +0700

    feat(reactive state): implement state usage in manual way

--- src/lib/State.svelte
+++ src/lib/State.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
-    // dalam svelte terdapat istilah rune yang digunakan sebagai pengontrol svelte compiler
-    // rune memiliki simbol awalan -> $
-    // contoh penggunaan rune (state) -> $state(parameter)
-    // sekilas dari yang terlihat seperti sebuah function atau method
+  // dalam svelte terdapat istilah rune yang digunakan sebagai pengontrol svelte compiler
+  // rune memiliki simbol awalan -> $
+  // contoh penggunaan rune (state) -> $state(parameter)
+  // sekilas dari yang terlihat seperti sebuah function atau method
 
-    let state: number = $state(0)
+  // reactive state adalah salah satu dari implementasi rune
+  // reactive state adalah state yang dapat merender ulang tampilan ketika state tersebut berubah
+  // perbedaan dengan state biasa adalah state biasa tidak dapat merender ulang tampilan ketika state tersebut berubah
 
-    function hitung() {
-        return state++
-    }
+  // contoh implementasi tanpa menggunakan reactive state
 
+  let hitungs: number = 0;
+
+  function increment() {
+    hitungs++;
+    document.getElementById('hitung').innerText = 'Jumlah : ' + hitungs;
+  }
+
+  let state: number = $state(0);
+
+  function hitung() {
+    return state++;
+  }
 </script>
 
+<p id="hitung">Jumlah : 0</p>
+
+<button onclick={increment}> hitung </button> <br />
+
 <button onclick={hitung}>
-    {state}
+  {state}
 </button>
