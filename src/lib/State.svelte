984b317b79b3c34a29351cd112a787273077b4a7
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-04-29 14:15:36 +0700

    feat(reactive state): implement reactive state for changing font color

--- src/lib/State.svelte
+++ src/lib/State.svelte
@@ -36,7 +36,64 @@
   function gantiWarnaBiru() {
     warnaFont = 'text-blue-500';
   }
+  let angka1: number = $state(0);
+  let angka2: number = $state(0);
+  let hasil: number = $state(0);
+
+  function tambah() {
+    hasil = angka1 + angka2;
+  }
+
+  function kurang() {
+    hasil = angka1 - angka2;
+  }
+
+  let nama: string = $state('');
+
+  function gantiNama() {
+    if (nama == '') {
+      nama = 'Ammar';
+    }
+  }
+
+  let warna = $state('');
+  let hasilWarna = $state('');
+
+  function gantiWarnaFont() {
+    if (warna == 'merah' || warna == 'red') {
+      hasilWarna = 'text-red-500';
+    } else if (warna == 'hijau' || warna == 'green') {
+      hasilWarna = 'text-green-500';
+    } else if (warna == 'biru' || warna == 'blue') {
+      hasilWarna = 'text-blue-500';
+    } else {
+      hasilWarna = 'text-black';
+      alert('Warna tidak valid');
+    }
+  }
 </script>
+
+<p class={`${hasilWarna} text-2xl font-bold`}>Ammar</p>
+
+<input type="text" bind:value={warna} placeholder="Masukkan warna" />
+
+<button
+  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  onclick={gantiWarnaFont}>
+  Ganti warna
+</button>
+
+<input type="number" bind:value={angka1} placeholder="Masukkan angka pertama" />
+<input type="number" bind:value={angka2} placeholder="Masukkan angka kedua" />
+<button onclick={tambah}>Tambah</button>
+<button onclick={kurang}>Kurang</button>
+
+<p>
+  Hasil : {hasil}
+</p>
+
+<p>Hello, {nama}</p>
+<button onclick={gantiNama}>Ganti Nama</button>
 
 <p id="hitung">Jumlah : 0</p>
 
