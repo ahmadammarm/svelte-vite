d613e36a2e0dca513ea25449a68bdcc53dbdde43
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-05-01 20:46:07 +0700

    feat(derived state): add the implementation of derived state lesson

--- src/lib/Derived.svelte
+++ src/lib/Derived.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
+  // derived state adalah state yang dihasilkan dari state lain
+  // misalkan
 
-    // derived state adalah state yang dihasilkan dari state lain
-    // misalkan
+  let hitung = $state(0);
+  let kali = $derived(hitung * 3);
 
-    let hitung = $state(0);
-    let kali = $derived(hitung * 3);
+  // jika state hitung berubah maka state kali yang merupakan derived state juga akan berubah
 
-    // jika state hitung berubah maka state kali yang merupakan derived state juga akan berubah
+  let array = $state([]);
+  let hitungs = $derived(array.reduce((a, b) => a + b, 0));
 
+  function increment() {
+    array.push(Math.floor(Math.random() * 10));
+  }
 </script>
 
-<button onclick={function() { hitung++; }}>
-    {kali}
+<button
+  onclick={function () {
+    hitung++;
+  }}>
+  {kali}
 </button>
 
 <p>
-    jika angka {hitung} dikali 3 maka hasilnya adalah {kali}``
+  jika angka {hitung} dikali 3 maka hasilnya adalah {kali}``
 </p>
+
+<h1>
+  {array.join(' + ')} = {hitungs}
+</h1>
+
+<button onclick={increment}> hitung </button>
